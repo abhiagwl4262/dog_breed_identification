1aec189412bfeeb7f5fbfd5f37f8f370dce46295
abhishek agrawal <abhiagwl4262@gmail.com>
2018-10-19 18:12:59 +0530

    working

--- training.py
+++ training.py
@@ -23,70 +23,18 @@ from keras.callbacks import ModelCheckpoint, EarlyStopping
 import resnet
 import keras
 
-KAGGLE_DS_DIR = "/home/abhishek/dog_breed_id_kaggle/data/"
-labels_file_path = os.path.join(KAGGLE_DS_DIR,"labels.csv")
-train_data_path  = os.path.join(KAGGLE_DS_DIR, "train/")
-
-## Analyse data
-train_dogs = pd.read_csv(labels_file_path)
-train_dogs.head()
-ax=pd.value_counts(train_dogs['breed'],ascending=True).plot(kind='barh',
-                                                       fontsize="5",
-                                                       title="Class Distribution",
-                                                       figsize=(25,50))
-ax.set(xlabel="Images per class", ylabel="Classes")
-ax.xaxis.label.set_size(40)
-ax.yaxis.label.set_size(40)
-ax.title.set_size(60)
-plt.show()
-
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-data_augmentation = False
-batch_size = 32
 num_classes = 120
+data_augmentation = True
+batch_size = 32
 epochs = 1000
 img_rows, img_cols, img_channels = 512, 512, 3
 input_shape = (img_rows, img_cols, img_channels)
 
-
-breeds = []
-breedCateogry = []
-j = 0 
-for i, (fname, breed) in train_dogs.iterrows():
-  if breed not in breeds:
-    breeds.append(breed)
-    breedCateogry.append(j)
-    j+=1
-
-images = []
-labels = []
-labels_num=[]
-for i, (fname, breed) in train_dogs.iterrows():
-  image_path = train_data_path + fname + ".jpg"
-  im = load_img(image_path, target_size=(512, 512))
-  im = img_to_array(im)
-  im = np.array(im).astype('float32')
-  images.append(im)
-  labels.append(breed)
-  for k in range(0, len(breeds)):
-    if(breeds[k] == breed):
-      labels_num.append(breedCateogry[k])
-
-print(len(images))
-print(len(labels_num))
-print(len(labels))
-
-num_classes = len(breeds)
-
-image_data = np.array(images)
-y_train = np.array(labels_num)
-labels_data = keras.utils.to_categorical(labels_num, num_classes)
+image_data  = np.load('image_data.npy')
+labels      = np.load('y_train.npy')
+labels_data = keras.utils.to_categorical(labels, num_classes)
 
 x_train, x_validation, y_train, y_validation = train_test_split(image_data, labels_data, test_size=0.2, stratify=np.array(labels), random_state=100)
-
-# num_train      = x_train.shape(0)
-# num_validation = x_validation.shape(0)
 
 
 model_path = './dog_breed_id_kaggle_firsttry.h5'
